00685e12108027d15547935c79bcf51b36ed4408
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2024-07-16 01:29:46 -0300

    config: Spring Boot base project

---
+++ my-new-adventure/src/main/java/com/mynewadventure/MyNewAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.mynewadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNewAdventureApplication.class, args);
+	}
+
+}
---
+++ my-new-adventure/src/test/java/com/mynewadventure/MyNewAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynewadventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNewAdventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
